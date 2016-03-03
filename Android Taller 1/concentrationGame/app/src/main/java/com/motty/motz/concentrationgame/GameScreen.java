b42a911e8bb8de94b9816aa465810385bd798669
Carlos Mottagit add * <Carlos93504@hotmail.com>
2016-03-03 00:15:49 -0500

    Added first template of the project created in Android Studio

---
+++ Android Taller 1/concentrationGame/app/src/main/java/com/motty/motz/concentrationgame/GameScreen.java
@@ -0,0 +1,20 @@
+package com.motty.motz.concentrationgame;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class GameScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_screen);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+}
