664946ee79d7cd38b06213eca9dfd4be461cabde
Carlos Mottagit add * <Carlos93504@hotmail.com>
2016-05-09 22:41:45 -0500

    Added baseline wpf project

---
+++ Proyecto Windows/ProyectoMoviles/Model/Contact.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoMoviles.Model
+{
+    public class Contact
+    {
+        public String Name { get; set; }
+        public String Username { get; set; }
+
+        public Contact(String Name, String Username)
+        {
+            this.Name = Name;
+            this.Username = Username;
+        }
+    }
+}
